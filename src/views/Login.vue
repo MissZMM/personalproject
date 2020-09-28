04c1c6f9afbe80c2b5a2425e4e3a95a54247b3cc
MissZMM <861915609@qq.com>
2020-09-28 14:39:28 +0800

    login01

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,21 @@
 <template>
-    <div>hahahah</div>
+    <div class="content">
+      <div class="loginBox">
+        <div class="title">登录</div>
+        <el-form label-position='left'>
+          <el-form-item prop="email" label="邮箱">
+            <el-input></el-input>
+          </el-form-item>
+          <el-form-item label="密码">
+            <el-input></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary">提交</el-button>
+            <el-button>重置</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
 </template>
 
 <script>
@@ -8,6 +24,25 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  .loginBox {
+    width: 500px;
+    height: 300px;
+    background-color: #EEFFCC;
+    border-radius: 8px;
+    box-sizing: border-box;
+    padding: 8px;
+    .title {
+      font-weight: bold;
+      font-size: 24px;
+      font-family: "Microsoft Himalaya";
+    }
+  }
+}
 </style>
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.sass$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  }
+}
