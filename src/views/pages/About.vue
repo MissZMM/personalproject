530ba2b6e6f788b5742af529e3c1c92973f42113
MissZMM <861915609@qq.com>
2020-10-20 21:05:55 +0800

    样式修改

--- src/views/pages/About.vue
+++ src/views/pages/About.vue
@@ -1,13 +1,25 @@
 <template>
   <div class="about">
-    <div class="header"></div>
+    <div class="header">
+        <div class="title">图像超分辨率</div>
+        <div class="loginInfo">
+            <el-dropdown class="drop">
+                <span class="el-dropdown-link">
+                  欢迎你， 张敏<i class="el-icon-arrow-down el-icon--right"></i>
+                </span>
+                <el-dropdown-menu slot="dropdown">
+                  <el-dropdown-item>退出登录</el-dropdown-item>
+                </el-dropdown-menu>
+            </el-dropdown>
+        </div>
+    </div>
     <div class="content">
       <el-menu
         :default-active="activeIndex"
         class="el-menu"
         mode="vertical"
         @select="handleSelect"
-        background-color="#545c64"
+        background-color="#312C28"
         text-color="#fff"
         active-text-color="#ffd04b">
         <el-menu-item index="1">我的工作台</el-menu-item>
@@ -60,9 +72,35 @@ export default {
   width: 100%;
   height: 100%;
   .header {
+      display: flex;
       width: 100%;
       height: 60px;
-      background-color: #554f55;
+      color: #FFFFFF;
+      box-sizing: border-box;
+      .title {
+          font-size: 26px;
+          width: 200px;
+          line-height: 60px;
+          background-color: #312C28;
+      }
+      .loginInfo {
+          position: relative;
+          width: calc(100% - 200px);
+          height: 40px;
+          line-height: 40px;
+          background-color: rgba(242, 237, 248, 0.3);
+          .drop {
+              position: absolute;
+              right: 10px;
+              top: 0;
+              .el-dropdown-link {
+                cursor: pointer;
+              }
+              .el-icon-arrow-down {
+                font-size: 12px;
+              }
+          }
+      }
   }
   .content {
     width: 100%;
